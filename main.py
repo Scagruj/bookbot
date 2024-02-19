4dd69be4a15088d9b676e9f9dc8a633611c6e237
Scagruj <joaojurgac@gmail.com>
2024-02-19 18:54:27 -0300

    created the main function of the bookbot

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+def main():
+    book = book_path("books/frankenstein.txt")
+    print(book)
+
+
+
+
+def book_path(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+
+
+main()
